fa64cb7e9c43f5044065a67d1c77a8ad3c07d5ce
Weblate <noreply@weblate.org>
2022-08-19 10:45:58 +0000

    Translated using Weblate (Russian)
    Currently translated at 97.8% (45 of 46 strings)
    
    Co-authored-by: Dmitriy <dimoon91@yandex.ru>
    Translate-URL: http://weblate.nubecula.org/projects/harbour-evento/harbour-evento/ru/
    Translation: harbour-evento/harbour-evento

--- translations/harbour-evento-ru.ts
+++ translations/harbour-evento-ru.ts
@@ -4,4 +4,187 @@
 <context>
     <name></name>
 </context>
+<message id="id-edit-event">
+    <source>Edit event</source>
+    <translation>Изменить событие</translation>
+</message>
+<message id="id-add-event">
+    <source>Add event</source>
+    <translation>Добавить событие</translation>
+</message>
+<message id="id-save">
+    <source>Save</source>
+    <translation>Сохранить</translation>
+</message>
+<message id="id-add">
+    <source>Add</source>
+    <translation>Добавить</translation>
+</message>
+<message id="id-title">
+    <source>Title</source>
+    <translation>Название</translation>
+</message>
+<message id="id-about">
+    <source>About</source>
+    <translation>О программе</translation>
+</message>
+<message id="id-translations">
+    <source>Translations</source>
+    <translation>Переводы</translation>
+</message>
+<message id="id-sources">
+    <source>Sources</source>
+    <translation>Исходники</translation>
+</message>
+<message id="id-donations">
+    <source>Donations</source>
+    <translation>На пиво</translation>
+</message>
+<message id="id-buy-me-a-beer">
+    <source>If you like my work why not buy me a beer?</source>
+    <translation>Если тебе нравится приложение, почему бы не угостить меня пивом?</translation>
+</message>
+<message id="id-donate-paypal">
+    <source>Donate with PayPal</source>
+    <translation>Через PayPal</translation>
+</message>
+<message id="id-donate-liberpay">
+    <source>Donate with Liberpay</source>
+    <translation>Через Librepay</translation>
+</message>
+<message id="id-main-title">
+    <source>Eventlist</source>
+    <translation>Список событий</translation>
+</message>
+<message id="id-delete">
+    <source>Delete</source>
+    <translation>Удалить</translation>
+</message>
+<message id="id-edit">
+    <source>Edit</source>
+    <translation>Изменить</translation>
+</message>
+<message id="id-view-placeholder-text">
+    <source>No events available</source>
+    <translation>Нет событий</translation>
+</message>
+<message id="id-view-placeholder-hint">
+    <source>Pull down to add new event</source>
+    <translation>Потяните вниз, чтобы добавить новое событие</translation>
+</message>
+<message id="id-failed-to-save">
+    <source>Failed to save events</source>
+    <translation>Не удалось сохранить событие</translation>
+</message>
+<message id="id-failed-to-load">
+    <source>Failed to load events</source>
+    <translation>Не удалось загрузить события</translation>
+</message>
+<message id="id-days-unit">
+    <source>d</source>
+    <extracomment>&quot;Unit of days&quot;</extracomment>
+    <translation>д</translation>
+</message>
+<message id="id-sort">
+    <source>Sort</source>
+    <translation>Сортировать</translation>
+</message>
+<message id="id-sort-by">
+    <source>Sort by</source>
+    <translation>Сортировать по</translation>
+</message>
+<message id="id-days-left">
+    <source>Days left</source>
+    <translation>Остаток дней</translation>
+</message>
+<message id="id-date">
+    <source>Date</source>
+    <translation>Дата</translation>
+</message>
+<message id="id-date-added">
+    <source>Date added</source>
+    <translation>Дата добавления</translation>
+</message>
+<message id="id-sort-order">
+    <source>Sort order</source>
+    <translation>Порядок сортировки</translation>
+</message>
+<message id="id-ascending">
+    <source>Ascending</source>
+    <translation>Поднимающийся</translation>
+</message>
+<message id="id-descending">
+    <source>Descending</source>
+    <translation>Спускающийся</translation>
+</message>
+<message id="id-sorting">
+    <source>Sorting</source>
+    <translation>Сортировка</translation>
+</message>
+<message id="id-days-until">
+    <source>Days until</source>
+    <oldsource>Day(s) until</oldsource>
+    <translation>Осталось</translation>
+</message>
+<message id="id-day-since">
+    <source>Days since</source>
+    <oldsource>Day(s) since</oldsource>
+    <translation>Прошло с тех пор</translation>
+</message>
+<message id="id-repeat">
+    <source>Repeat</source>
+    <translation>Повторять</translation>
+</message>
+<message id="id-weekly">
+    <source>weekly</source>
+    <translation>еженедельно</translation>
+</message>
+<message id="id-monthly">
+    <source>monthly</source>
+    <translation>ежемесячно</translation>
+</message>
+<message id="id-yearly">
+    <source>yearly</source>
+    <translation>ежегодно</translation>
+</message>
+<message id="id-settings">
+    <source>Settings</source>
+    <translation>Настройки</translation>
+</message>
+<message id="id-cover-auto-switch">
+    <source>Auto switch</source>
+    <translation>Автопереключение</translation>
+</message>
+<message id="id-cover-switch-interval-title">
+    <source>Interval (msec)</source>
+    <translation>Включите автоматическое переключение между событиями на титульной странице с определенным интервалом (мсек)</translation>
+</message>
+<message id="id-app-name">
+    <source>Evento</source>
+    <translation>Эвенто</translation>
+</message>
+<message id="id-title-placeholder">
+    <source>Enter title</source>
+    <translation>Введите название</translation>
+</message>
+<message id="id-about-page-description">
+    <source>Sailfish OS app to count remaining days to defined events .</source>
+    <translation>Приложение для Sailfish OS, подсчитывающее количество дней до заданных событий.</translation>
+</message>
+<message id="id-about-page-translation-text">
+    <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
+    <translation>Ваш язык недоступен? Вы можете поддержать этот проект, переведя его на Weblate.</translation>
+</message>
+<message id="id-sort-dialog-title">
+    <source>Change sorting</source>
+    <translation>Изменить сортировку</translation>
+</message>
+<message id="id-never">
+    <source>never</source>
+    <translation>никогда</translation>
+</message>
+<message id="id-cover-auto-switch-desc">
+    <source>Turn on automatic switch between events on cover page in a defined interval</source>
+    <translation>Включите автопереключение событий на титульной странице с определенным интервалом</translation>
+</message>
 </TS>
